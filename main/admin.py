8cbe6c8d96096b035714bc25406870ce39e92eaa
telanxvt <telanxvt@gmail.com>
2013-12-05 01:23:36 -0500

    working on compound model

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from main.models import project, data, experiment, readout, fileformat, plate
+from main.models import compound, library_info,chemical_info
 
 # Register your models here.
 
@@ -9,3 +10,6 @@ admin.site.register(experiment)
 admin.site.register(readout)
 admin.site.register(fileformat)
 admin.site.register(plate)
+admin.site.register(compound)
+admin.site.register(library_info)
+admin.site.register(chemical_info)
