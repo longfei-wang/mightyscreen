5cb37d2fcb374f471a6d706b1c83a8bf8af0c10d
longfei-wang <wanglf1983@gmail.com>
2013-12-10 11:37:47 -0500

    Update models.py

--- data/models.py
+++ data/models.py
@@ -6,12 +6,13 @@ from main.models import data_base,project
 #figured that the table would be too big to store all the data. 
 #A HTS project can easily have millions of entries. Better handle project seperately
 for i in project.objects.all():
-    n=''
-    for j in i.experiment.readout.all():
+    if i.experiment.readout.all():
+        n=''
+        for j in i.experiment.readout.all():
 
-        n+=j.name+'=models.TextField();'
+            n+=j.name+'=models.TextField();'
 
-    exec ('class proj_'+str(i.pk)+'(data_base):'+n)
+        exec ('class proj_'+str(i.pk)+'(data_base):'+n)
 
 
 
