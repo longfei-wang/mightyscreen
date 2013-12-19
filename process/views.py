21783eb42fa34beeb0b1bbe016a0b280414dfae1
longfei.wang <wanglf1983@gmail.com>
2013-12-18 19:39:02 -0500

    css js fonts for bootstrap

--- process/views.py
+++ process/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.core.context_processors import csrf
-from main.models import project
+from main.models import project, data_base
 # Create your views here.
 
 def index(request):
@@ -8,5 +8,5 @@ def index(request):
     
 def mark(request):
     proj=project.objects.get(pk=request.session['proj_id'])
-    
-    return render(request,'process/markwell.html',{'proj':proj})
+    welltypes=dict(data_base.schoice)
+    return render(request,'process/markwell.html',{'proj':proj,'welltypes':welltypes})
