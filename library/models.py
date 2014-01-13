4162132a00bf97b9bae962b69321395f701da12c
longfei-wang <wanglf1983@gmail.com>
2014-01-12 20:51:12 -0500

    compound model

--- library/models.py
+++ library/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from mongoengine import *
 
 # Create your models here.
 
@@ -11,47 +12,44 @@ from django.db import models
 #        return self.readout
 #    
 #    datafile = models.FileField(upload_to = 'rawdata_test_%Y%m%d')
-    
 
-class compound(models.Model):
+
+class library(Document):
+    
+    def __unicode__(self):
+        return self.library_name
+
+    name = StringField(max_length = 30)
+    description=StringField(max_length=200)
+
+
+class compound(Document):
+    
     def __unicode__(self):
         return (self.plate_well)
 
     hidden_fields=['plate_well','sdf','id','fp2','fp3','fp4','plate','well']
 
-    library_name = models.ForeignKey('library')
-    sub_library_name = models.ForeignKey('sub_library')
-    facility_reagent_id = models.CharField(max_length = 30)
-    plate = models.IntegerField(default=0)
-    well = models.CharField(max_length = 20)
-    plate_well = models.CharField(max_length = 20)
-    pubchem_cid = models.IntegerField(default=0)
+    library_name = ReferenceField(library)
+    sub_library_name = StringField(max_length=50)
+    facility_reagent_id = StringField(max_length = 30)
+    plate = IntField()
+    well = StringField(max_length = 20)
+    plate_well = StringField(max_length = 20)
+    pubchem_cid = IntField()
     chemical_name = models.TextField()
-    molecular_weight = models.DecimalField(max_digits=10, decimal_places=5)
-    formula = models.CharField(max_length = 30)
-    tpsa = models.DecimalField(max_digits=8, decimal_places=2)
-    logp = models.DecimalField(max_digits=8, decimal_places=4)
-    inchikey = models.CharField(max_length = 30)
-    canonical_smiles = models.TextField()
-    inchi = models.TextField()
-    fp2 = models.TextField()
-    fp3 = models.CharField(max_length = 50)
-    fp4 = models.TextField()
-    svg = models.TextField(verbose_name='Structure')
-    sdf = models.TextField()
+    molecular_weight = DecimalField()
+    formula = StringField(max_length = 30)
+    tpsa = DecimalField()
+    logp = DecimalField()
+    inchikey = StringField(max_length = 30)
+    canonical_smiles = StringField()
+    inchi = StringField()
+    fp2 = StringField()
+    fp3 = StringField(max_length = 50)
+    fp4 = StringField()
+    svg = StringField(verbose_name='Structure')
+    sdf = StringField()
 
 
     
-class sub_library(models.Model):
-    def __unicode__(self):
-        return self.sub_library_name 
-    sub_library_name = models.CharField(max_length = 30)
-    super_library = models.ForeignKey('library')
-    number_of_compounds = models.IntegerField(default=0)
-
-class library(models.Model):
-    def __unicode__(self):
-        return self.library_name
-    library_name = models.CharField(max_length = 30)
-    number_of_sub_librarys = models.IntegerField(default=0)
-    number_of_compounds = models.IntegerField(default=0)
