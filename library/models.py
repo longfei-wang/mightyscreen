a952f6d13756b129f8890c300e78358915ec8f1e
telanxvt <telanxvt@gmail.com>
2014-01-13 23:50:19 -0500

    upload, referencefield not resolved

--- library/models.py
+++ library/models.py
@@ -18,8 +18,15 @@ class library(Document):
     def __unicode__(self):
         return self.library_name
 
-    name = StringField(max_length = 30)
+    library_name = StringField(max_length = 30)
     description=StringField(max_length=200)
+    
+    meta = {
+    'indexes': [
+        {'fields': ['library_name'], 'unique': True,
+          'sparse': False, 'types': False },
+    ],
+    }
 
 
 class compound(Document):
@@ -29,13 +36,13 @@ class compound(Document):
 
     hidden_fields=['plate_well','sdf','id','fp2','fp3','fp4','plate','well']
 
-    library_name = ReferenceField(library)
+    library_name = ReferenceField(library, dbref=True)
     sub_library_name = StringField(max_length=50)
     facility_reagent_id = StringField(max_length = 30)
     plate = StringField(max_length=20)
     well = StringField(max_length = 20)
     plate_well = StringField(max_length = 20)
-    pubchem_cid = IntField()
+    pubchem_cid = StringField(max_length = 20)
     chemical_name = StringField()
     molecular_weight = DecimalField()
     formula = StringField(max_length = 30)
@@ -51,4 +58,9 @@ class compound(Document):
     sdf = StringField()
 
 
-    
+    meta = {
+    'indexes': [
+        {'fields': ['plate_well'], 'unique': True,
+          'sparse': True, 'types': False },
+    ],
+    }    
