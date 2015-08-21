a60dabb092af1a76e6d48c23539ccf3ac6a2dc17
Longfei Wang <wanglf1983@gmail.com>
2015-08-21 12:51:11 -0400

    production

--- mightyscreen/settings.py
+++ mightyscreen/settings.py
@@ -20,11 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'gl=^$l1e&ya7qbznhg(#x*w9$f+^_kv(_q9c_k&oshpb%@6qf2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mightyscreen.net','mightyscreen.sbgrid.org']
 
 
 # Application definition
