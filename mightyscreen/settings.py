26c435860640a822f70103036e335b5c8c469f27
longfei <wanglf1983@gmail.com>
2015-08-31 13:29:18 -0400

    grid sys for plateview

--- mightyscreen/settings.py
+++ mightyscreen/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'compressor',
     #'django_filters',
     'crispy_forms',
     #'library',
